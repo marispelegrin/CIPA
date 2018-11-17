58b538554022a34d0c8792122131723231cf56a2
Maristela Marquiafave Pelegrin <43457630+marispelegrin@users.noreply.github.com>
2018-11-17 20:53:17 -0200

    Update contrato1.sol

--- contrato/contrato1.sol
+++ contrato/contrato1.sol
@@ -12,12 +12,12 @@ contract EleicaoCipa {
         return totalVotos[candidato];
     }
     function hilary(bytes32 candidato){
-      if(validCandidate(candidate)==false) throw;
-        totalVotes[candidate]+=1;
+      if(candidatoValido(candidato)==false) throw;
+        totalVotos[candidato]+=1;
     }
-    function validCandidate(bytes32 candidate) returns (boolen){
-        for(uint i =0;i<candidatesList.length;i++){
-            if(candidates[i]==candidate){
+    function candidatoValido(bytes32 candidate) returns (boolen){
+        for(uint i =0;i<listaDeCandidatos.length;i++){
+            if(candidatos[i]==candidate){
             return true;
             }
         }
