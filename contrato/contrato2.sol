e2ea25a3243e63b4f41b71c7bc55ea392c61c17e
Maristela Marquiafave Pelegrin <43457630+marispelegrin@users.noreply.github.com>
2018-11-17 20:45:16 -0200

    Votação para membros da CIPA de uma empresa

---
+++ contrato/contrato2.sol
@@ -0,0 +1,44 @@
+pragma  solidity 0.4.25;
+
+contract eleiçãoCipa {
+    struct candidato {
+        uint id;
+        string name;
+        uint contagemVotos;
+    }
+
+    // Armazena as contas que já votaram
+    mapping(address => bool) public votantes;
+    // Armazena lista de candidatos
+    // Busca o candidato
+    mapping(uint => Candidato) public candidatos;
+    // Armazena contagem dos candidatos
+    uint public contagemCandidatos;
+
+    event votou(
+        uint indexed _candidatoId
+    );
+
+    function Eleição() public {
+        addCandidato("Donald Trump");
+        addCandidato("Hillary Clinton");
+    }
+
+    function addCandidate(string _name) private {
+        contagemCandidatos++;
+        candidatos[contagemCandidatos] = Candidato(contagemCandidatos, _name, 0);
+    }
+
+    function votar (uint _candidatoId) public {
+        // Requer que o votante não tenha votado antes 
+        require(!votantes[msg.sender]);
+        // Requer um candidato válido
+        require(_candidatoId > 0 && _candidatoId <= contagemCandidatos);
+        // Grava o voto
+        votantes[msg.sender] = true;
+        // Atualiza a contagem dos votos dos candidatos
+        candidatos[_candidatoId].contagemCandidatos++; 
+        // Dispara a condição de votou
+        emit votou(_candidatoId);
+    }
+}
