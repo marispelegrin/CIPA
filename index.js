edeffbab80aef4a61fe9be05ae99efc9022ad7e7
Maristela Marquiafave Pelegrin <43457630+marispelegrin@users.noreply.github.com>
2018-11-18 13:46:21 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+function calcularValor() {
+    
+    var valorBase = document.formDecTerc.valorSalario.value;
+	var meses = document.formDecTerc.meses.value;
+	
+    var valorCalculado;   
+        if (meses == "janeiro") {
+            valorCalculado = valorBase;
+        } else if (meses == "fevereiro") {
+            valorCalculado = valorBase/12*11;
+        } else if (meses == "marco") { 
+            valorCalculado = valorBase/12*10;
+        } else if (meses == "abril") {
+            valorCalculado = valorBase/12*9;
+        } else if (meses == "maio") {
+            valorCalculado = valorBase/12*8;
+        } else if (meses == "junho") {
+            valorCalculado = valorBase/12*7;
+        } else if (meses == "julho") {
+            valorCalculado = valorBase/12*6;
+        }else if (meses == "agosto") {
+            valorCalculado = valorBase/12*5; 
+        } else if (meses == "setembro") {
+            valorCalculado = valorBase/12*4;       
+        } else if (meses == "outubro") {
+            valorCalculado = valorBase/12*3;
+        } else if (meses == "novembro") {
+            valorCalculado = valorBase/12*2;
+        } else if (meses == "dezembro") {
+            valorCalculado = valorBase/12;
+        } 
+        var valorFinal = "Seu décimo terceiro salário <br>" + "Total (R$):" + valorCalculado;
+        document.getElementById("calculo").innerHTML = valorFinal;
+		}
+
+
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:3000"));
 abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
 VotingContract = web3.eth.contract(abi);
