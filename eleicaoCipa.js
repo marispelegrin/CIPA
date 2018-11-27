579d30ed3deaeca8948c72cc00918373730dac1f
Maristela Marquiafave Pelegrin <43457630+marispelegrin@users.noreply.github.com>
2018-11-27 20:41:40 -0200

    Create eleicaoCipa.js

---
+++ eleicaoCipa.js
@@ -0,0 +1,75 @@
+const EleicaoCipaABI = [
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "nomeEmpresa",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "receberPeloUso",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "qualAgente",
+				"type": "address"
+			}
+		],
+		"name": "definirAgente",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "qualNomeDaEmpresa",
+				"type": "string"
+			}
+		],
+		"name": "definirNomeDaEmpresa",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	}
+];
+
+var contratoUsoDeImagem = web3.eth.contract(contratoUsoDeImagemABI).at("0xba62377d64aaa6c5fb69562be537b91e30e183e4");
+
+function obtemNomeEmpresa() {
+    contratoUsoDeImagem.nomeEmpresa({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+        if (err)    {
+            console.log("Erro");
+            console.error(err);
+        } else {
+            console.log("Resultado");
+            let objStatus = document.getElementById("spanNomeEmpresa");
+            console.log(resultado);
+            objStatus.innerText = resultado;
+        }
+    });
+}
