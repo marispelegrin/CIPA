c19da784eca2f64cd5442a6a358fb1c7a79899cf
Maristela Marquiafave Pelegrin <43457630+marispelegrin@users.noreply.github.com>
2018-11-27 12:55:57 -0200

    Update contrato.sol

--- contrato/contrato.sol
+++ contrato/contrato.sol
@@ -1,9 +1,9 @@
-pragma  solidity 0.4.25;
+pragma  solidity ^0.4.25;
 
 contract eleicaoCipa { 
     struct candidato {
         uint id;
-        string name;
+        string nome;
         uint contagemVotos;
     }
 
